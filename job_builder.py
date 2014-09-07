aa1fca46bb7d70c74d8a38389872cc6faa4dbd3a
ubuntu <shamil.ishraq@gmail.com>
2014-09-07 19:23:47 -0400

    remove tabs

--- job_builder.py
+++ job_builder.py
@@ -1,44 +1,44 @@
 from mapreduce.job import Job
 
 class JobBuilder():
-	def __init__(self):
-		self.args = {}
+  def __init__(self):
+    self.args = {}
 
-	def set_name(self, name):
-		self.args['name'] = name
-		return self
-	
-	def set_mapper(self, map_func):
-		self.args['map_func'] = map_func
-		return self
-	
-	def set_reducer(self, reduce_func):
-		self.args['reduce_func'] = reduce_func
-		return self
-	
-	def set_input_dir(self, dir):
-		self.args['input_dir'] = dir
-		return self
-	
-	def set_working_dir(self, dir):
-		self.args['working_dir'] = dir
-		return self 
-	
-	def set_output_dir(self, dir):
-		self.args['output_dir'] = dir
-		return self
-	
-	def set_mapper_count(self, count):
-		self.args['mapper_count'] = count
-		return self
-		
-	def set_reducer_count(self, count):
-		self.args['reducer_count'] = count
-		return self
-	
-	def set_sort_key_provider(self, func):
-		self.args['sort_key_provider'] = func
-		return self
-	
-	def build(self):
-		return Job(**self.args)
+  def set_name(self, name):
+    self.args['name'] = name
+    return self
+  
+  def set_mapper(self, map_func):
+    self.args['map_func'] = map_func
+    return self
+  
+  def set_reducer(self, reduce_func):
+    self.args['reduce_func'] = reduce_func
+    return self
+  
+  def set_input_dir(self, dir):
+    self.args['input_dir'] = dir
+    return self
+  
+  def set_working_dir(self, dir):
+    self.args['working_dir'] = dir
+    return self 
+  
+  def set_output_dir(self, dir):
+    self.args['output_dir'] = dir
+    return self
+  
+  def set_mapper_count(self, count):
+    self.args['mapper_count'] = count
+    return self
+    
+  def set_reducer_count(self, count):
+    self.args['reducer_count'] = count
+    return self
+  
+  def set_sort_key_provider(self, func):
+    self.args['sort_key_provider'] = func
+    return self
+  
+  def build(self):
+    return Job(**self.args)
--- job_runner.py
+++ job_runner.py
@@ -3,99 +3,99 @@ from os import path, walk
 import simplejson as json
 
 class JobRunner():
-	def run(self, job):
-		print("Starting job: ", job.name)
-		mapper = Mapper(job)
-		interm_data = mapper.run()
-		interm_data = self.__sort(job, interm_data)
-		reducer = Reducer(job)
-		return reducer.run(interm_data)
-	
-	def __sort(self, job, data):
-		if not job.sort_key_provider:
-			key_provider = lambda x: x[0]
-		else:
-			key_provider = lambda x: job.sort_key_provider(x[0])
-		return sorted(data, key=key_provider)
-	
+  def run(self, job):
+    print("Starting job: ", job.name)
+    mapper = Mapper(job)
+    interm_data = mapper.run()
+    interm_data = self.__sort(job, interm_data)
+    reducer = Reducer(job)
+    return reducer.run(interm_data)
+  
+  def __sort(self, job, data):
+    if not job.sort_key_provider:
+      key_provider = lambda x: x[0]
+    else:
+      key_provider = lambda x: job.sort_key_provider(x[0])
+    return sorted(data, key=key_provider)
+  
 
 class Mapper():
-	"""
-		All keys must be hashable
-	"""
-	
-	def __init__(self, job):
-		self.job = job
-	
-	def map_wrapper(self, file):
-		print("Mapping file: ", path.basename(file))
-		result = {}
-		for key, value in self.job.map_func(file):
-			if key in result:
-				result[key].append(value)
-			else:
-				result[key] = [value]
-		return result
-	
-	def __flatten(self, l):
-		return [ item for sublist in l for item in sublist ]
-	
-	def __merge_results(self, results):
-		merged = {}
-		keys = set(self.__flatten([ r.keys() for r in results ]))
-		for k in keys:
-			for r in results:
-				if k in r:
-					if k in merged:
-						merged[k] += r[k]
-					else:
-						merged[k] = r[k]
-		return merged
-	
-	def __get_input_files(self):
-		basedir = self.job.input_dir
-		files = walk(basedir).__next__()[2]
-		return [ path.join(basedir, f) for f in files ]
-					
-		
-	def run(self):
-		""" 
-			Returns a list of tuples [(key, value)...]
-		"""
-		pool = Pool(processes=self.job.mapper_count,)
-		input_files = self.__get_input_files()
-		print("Total files: ", len(input_files))
-		results = pool.map(self.map_wrapper, iter(input_files))
-		results = self.__merge_results(results)
-		self.results = list(results.items())
-		return self.results
+  """
+    All keys must be hashable
+  """
+  
+  def __init__(self, job):
+    self.job = job
+  
+  def map_wrapper(self, file):
+    print("Mapping file: ", path.basename(file))
+    result = {}
+    for key, value in self.job.map_func(file):
+      if key in result:
+        result[key].append(value)
+      else:
+        result[key] = [value]
+    return result
+  
+  def __flatten(self, l):
+    return [ item for sublist in l for item in sublist ]
+  
+  def __merge_results(self, results):
+    merged = {}
+    keys = set(self.__flatten([ r.keys() for r in results ]))
+    for k in keys:
+      for r in results:
+        if k in r:
+          if k in merged:
+            merged[k] += r[k]
+          else:
+            merged[k] = r[k]
+    return merged
+  
+  def __get_input_files(self):
+    basedir = self.job.input_dir
+    files = walk(basedir).__next__()[2]
+    return [ path.join(basedir, f) for f in files ]
+          
+    
+  def run(self):
+    """ 
+      Returns a list of tuples [(key, value)...]
+    """
+    pool = Pool(processes=self.job.mapper_count,)
+    input_files = self.__get_input_files()
+    print("Total files: ", len(input_files))
+    results = pool.map(self.map_wrapper, iter(input_files))
+    results = self.__merge_results(results)
+    self.results = list(results.items())
+    return self.results
 
 
 class Reducer():
 
-	def __init__(self, job):
-		self.job = job
-	
-	def __chunks(self, l, n):
-		for i in range(0, len(l), n):
-			yield l[i:i+n]
-			
-	def reduce_wrapper(self, data):
-		# TODO: make name for meaningful
-		filename = str(current_process().pid) + ".txt"
-		results = []
-		with open(path.join(self.job.output_dir, filename), 'w') as f:
-			for d in self.job.reduce_func(data):
-				results.append(d)
-				f.write(json.dumps(d) + "\n")
-		return results
-	
-	def run(self, data):
-		"""
-			Returns list of results from each reducer [[Reducer 1], [Reducer 2], ...]
-		"""
-		pool = Pool(processes=self.job.reducer_count,)
-		print("Running reduce on ", len(data), " records")
-		chunk_size = int(len(data) / self.job.reducer_count)
-		self.results = pool.map(self.reduce_wrapper, self.__chunks(data, chunk_size))
-		return self.results
+  def __init__(self, job):
+    self.job = job
+  
+  def __chunks(self, l, n):
+    for i in range(0, len(l), n):
+      yield l[i:i+n]
+      
+  def reduce_wrapper(self, data):
+    # TODO: make name for meaningful
+    filename = str(current_process().pid) + ".txt"
+    results = []
+    with open(path.join(self.job.output_dir, filename), 'w') as f:
+      for d in self.job.reduce_func(data):
+        results.append(d)
+        f.write(json.dumps(d) + "\n")
+    return results
+  
+  def run(self, data):
+    """
+      Returns list of results from each reducer [[Reducer 1], [Reducer 2], ...]
+    """
+    pool = Pool(processes=self.job.reducer_count,)
+    print("Running reduce on ", len(data), " records")
+    chunk_size = int(len(data) / self.job.reducer_count)
+    self.results = pool.map(self.reduce_wrapper, self.__chunks(data, chunk_size))
+    return self.results
